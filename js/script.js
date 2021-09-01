ab7e83aab6f6d66ca17dca9756980f0d94e2dd0d
Eliana-Abate <eliana.abate@outlook.it>
2021-09-01 15:29:26 +0200

    boilerplate

---
+++ js/script.js
@@ -0,0 +1,15 @@
+Vue.config.devtools = true;
+
+console.log ('collegato', Vue);
+
+const app = new Vue({
+    el: '#root',
+    data: {
+      email: '',
+    },
+    methods: {},
+    created() {
+      axios
+        
+    },
+  });
